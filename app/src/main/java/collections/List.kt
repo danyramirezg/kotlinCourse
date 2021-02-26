f84fd521ee2c80210a81a64378e97895cf9b9fde
danyramirezg <danyramirezg@gmail.com>
2021-02-26 11:29:38 -0500

    List immutables

---
+++ app/src/main/java/collections/List.kt
@@ -0,0 +1,34 @@
+package collections
+
+import java.nio.file.Files.size
+
+/*
+    - list is an ordered collection that can contain duplicate elements
+    - The empty list must specify the element type:
+        var colors = listOf<String>()
+
+    - The list are immutable, we can't add elements, we can't change the list
+
+     - We can access throw the index and get:
+        colors[0]
+
+    - Retrieve the size of the list
+        colors.size
+
+    - The list can contain null elements
+ */
+
+fun main() {
+
+    // The list can contain null elements:
+    val colors = listOf("Yellow", "blue", "red", null, null)
+    println(colors) // [Yellow, blue, red, null, null]
+
+    // Accessing to the elements, throw the index and get
+    println(colors[0])
+    println(colors.get(1))
+
+    // Retrieve the size of the list
+    println(colors.size)
+
+}
