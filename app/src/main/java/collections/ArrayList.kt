6ae774e26a8b82d4ebf23cf14c22f53613747314
danyramirezg <danyramirezg@gmail.com>
2021-02-26 12:12:54 -0500

    Array list are mutable

--- app/src/main/java/collections/ArrayList.kt
+++ app/src/main/java/collections/ArrayList.kt
@@ -1,2 +1,38 @@
 package collections
 
+/*
+    - Array list are mutable, I can add and remove elements
+    - Tip: If the list contain duplicate elements, the remove function only removes the first element
+    - I can add a list and remove a list to an Array list
+
+ */
+
+fun main() {
+    var colors: ArrayList<String> = arrayListOf("blue", "red")
+    println(colors)
+
+    var noColors = ArrayList<String>()
+    println(noColors) // []
+
+    // Adding elements:
+    colors.add("yellow")
+    println(colors)
+
+    // Adding another collection:
+    val moreColors = arrayListOf("pink", "white")
+    colors.addAll(moreColors)
+    println(colors) // [blue, red, yellow, pink, white]
+
+    // Remove an element and remove at position (index)
+    colors.remove("blue")
+    println(colors)
+
+    colors.removeAt(1)
+    println(colors)
+
+    // Remove all elements of a collection
+    colors.removeAll(moreColors)
+    println(colors)
+
+
+}
