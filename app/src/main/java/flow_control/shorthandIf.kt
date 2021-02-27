97db5c18260c3796a81b0c0e3b9bf1964fc15065
danyramirezg <danyramirezg@gmail.com>
2021-02-26 22:52:58 -0500

    Short hand if

---
+++ app/src/main/java/flow_control/shorthandIf.kt
@@ -0,0 +1,34 @@
+package flow_control
+
+/*
+    Shorthand if statement:
+
+    Curly brackets can be omitted if there is only one expression
+    An if statement can return a result
+ */
+
+fun main() {
+
+    var number = 40
+
+    val isEven = if (number % 2 == 0) "Number is even" else "Number is odd"
+
+    println(isEven)
+
+    /*
+    Exercise:
+        Read an integer from the console
+        if it has a single digit, print out a message
+        Otherwise, print out a different message
+        Use the shorthand if statement
+     */
+
+    println("Write a number")
+    val numb = readLine() ?: ""
+    var input = numb.toInt()
+    println(input::class.java)
+
+    val result = if (input < 10) "This is the number $input" else "The number has to be a single digit"
+
+    println(result)
+}
