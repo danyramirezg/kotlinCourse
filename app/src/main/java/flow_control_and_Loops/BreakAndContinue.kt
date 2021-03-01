d883a5fd8ad8c513fb246fbd1fd80cc9502653fc
danyramirezg <danyramirezg@gmail.com>
2021-02-28 20:23:47 -0500

    Break and continue

--- app/src/main/java/flow_control_and_Loops/BreakAndContinue.kt
+++ app/src/main/java/flow_control_and_Loops/BreakAndContinue.kt
@@ -1,2 +1,32 @@
 package flow_control_and_Loops
 
+/*
+    Exercise: Break and continue
+    A group of young people are going to a nightclub.
+
+    Design a program that accepts user ages.
+
+    The program displays a welcome message for each user.
+
+    If it receives an age lower than 18, it prints a message that this client is not allowed.
+
+    If it receives the word “stop”, the program ends
+
+ */
+
+fun main() {
+
+    while (true) {
+        println("Please enter your age")
+        val input = readLine() ?: ""
+        if (input == "stop")
+            break
+
+        val age = input.toInt()
+        if (age <= 18) {
+            println("The client is not allowed to enter")
+            continue
+        }
+        println("Welcome to the club")
+        }
+    }
