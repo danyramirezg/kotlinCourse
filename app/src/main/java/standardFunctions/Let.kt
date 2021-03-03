009910ea8dcb86549a4cd9bb5e021bb205dfb3d5
danyramirezg <danyramirezg@gmail.com>
2021-03-03 18:40:07 -0500

    let-Scope-function

---
+++ app/src/main/java/standardFunctions/Let.kt
@@ -0,0 +1,33 @@
+package standardFunctions
+
+/*
+    Exercise: Let
+    Read client names from the console until the word stop is introduced.
+    If the string is empty, the client is null.
+
+    Print out a hello message for each client.
+ */
+
+fun main() {
+    val clients = arrayListOf<String?>()
+
+    do {
+        println("Write client name:")
+        val input = readLine()
+        input?.let {
+            if (it.toLowerCase() != "stop") {
+                if (it == "")
+                    clients.add(null)
+                else
+                    clients.add(it)
+            }
+        }
+    } while (input?.toLowerCase() != "stop")
+    println(clients)
+
+    clients.forEach {
+        it?.let {
+            println("Hello $it")
+        }
+    }
+}
