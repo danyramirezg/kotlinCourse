0a758d64cc9ceb435e9f3807e4e1722a4bbfab85
danyramirezg <danyramirezg@gmail.com>
2021-03-03 18:40:37 -0500

    with-Scope-function

--- app/src/main/java/standardFunctions/With.kt
+++ app/src/main/java/standardFunctions/With.kt
@@ -1,2 +1,31 @@
 package standardFunctions
 
+/*
+
+    Exercise: With
+    A store has a supply of shoes, shirts and jackets, as well as a method to print out
+    the inventory.
+
+    Create a variable of type store, update its stock and print out the inventory.
+
+ */
+
+fun main() {
+    val store = Store()
+    with(store) {
+        shoes = 4
+        shirts = 5
+        jackets = 3
+        printInventory()
+    }
+}
+
+class Store {
+    var shoes = 0
+    var shirts = 0
+    var jackets = 0
+
+    fun printInventory() {
+        println("The store has $shoes pair of shoes, $shirts shirts and $jackets jackets")
+    }
+}
