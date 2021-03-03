acc62877ba56b41f961e37e43efd81b2609ea3da
danyramirezg <danyramirezg@gmail.com>
2021-03-03 11:01:38 -0500

    Encapsulation -Example protected modifier

--- app/src/main/java/anotherCourseObjectOriented/Encapsulation.kt
+++ app/src/main/java/anotherCourseObjectOriented/Encapsulation.kt
@@ -5,8 +5,27 @@ package anotherCourseObjectOriented
 
     A famous fast food restaurant has a secret formula for their burgers.
 
-    A franchise of this brand can sell burgers, but does not have access to the formula. They have to ask the original restaurant how to prepare the food.
+    A franchise of this brand can sell burgers, but does not have access to the formula. They have
+    to ask the original restaurant how to prepare the food.
 
     Implement this in a program, and call the franchise object to provide you with burgers.
 
  */
+
+fun main() {
+    val restaurant = Franchise()
+    restaurant.prepareBurgers()
+}
+
+open class OriginalRestaurant {
+    protected fun secretFormula(): String {
+        return "The secret is the love"
+    }
+}
+
+class Franchise : OriginalRestaurant() {
+    fun prepareBurgers() {
+        println("Bread, protein, lettuce and tomatoes")
+        println(secretFormula())
+    }
+}
