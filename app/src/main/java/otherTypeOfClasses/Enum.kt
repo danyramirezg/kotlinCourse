0ab77c853da9f3fc1485bfe61b00da5fa22edc0a
danyramirezg <danyramirezg@gmail.com>
2021-03-04 16:07:29 -0500

    Enum class

--- app/src/main/java/otherTypeOfClasses/Enum.kt
+++ app/src/main/java/otherTypeOfClasses/Enum.kt
@@ -1,2 +1,20 @@
 package otherTypeOfClasses
 
+fun main() {
+
+    val color = Colors.BLUE
+    when (color) {
+        Colors.BLUE -> println("You chose BLUE")
+        Colors.RED -> println("You chose RED")
+        Colors.YELLOW -> println("You chose YELLOW")
+    }
+    println(Colors.YELLOW.timeUsed) // 2
+    println(Colors.BLUE.name) // BLUE
+    println(Colors.RED.ordinal) // 0 (ordinal is the position)
+}
+
+enum class Colors(val timeUsed: Int) {
+    RED(34),
+    BLUE(12),
+    YELLOW(2)
+}
