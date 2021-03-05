73d254a2989abcd3898be54280f48e52a8b7299b
danyramirezg <danyramirezg@gmail.com>
2021-03-05 10:36:02 -0500

    Extensions functions

--- app/src/main/java/functions/ExtensionsCompanionObject.kt
+++ app/src/main/java/functions/ExtensionsCompanionObject.kt
@@ -1,2 +1,16 @@
 package functions
 
+/*
+    Exercise: Companion object extensions
+    Add a companion object function to the Double class that prints out the class name.
+
+    Call the companion object extension function.
+ */
+
+fun main() {
+    Double.getClassName()
+}
+
+fun Double.Companion.getClassName() {
+    println("The class is a double")
+}
--- app/src/main/java/functions/ExtensionsFunctions.kt
+++ app/src/main/java/functions/ExtensionsFunctions.kt
@@ -1,2 +1,26 @@
 package functions
 
+import kotlin.math.E
+
+/*
+
+Extension functions:
+
+    Can add functions to classes that we don't own or cannot modify
+    Can access the object using the 'this' reference
+
+Exercise:
+    Add an extension function to the ArrayList class that returns a message saying how many
+    elements the list contains.
+
+    Create an ArrayList and print out the extension function message.
+ */
+
+fun main() {
+    var list = arrayListOf("Caro", "Dani", "Dan")
+    println(list.getElements())
+}
+
+fun ArrayList<String>.getElements() = "The list contains $size elements"
+
+// Output: The list contains 3 elements
